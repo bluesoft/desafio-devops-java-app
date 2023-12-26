2b0f056f5893c106939df066a9128f5cb8003448
Daniel Faria Gomes <daniel.faria@bluesoft.com.br>
2023-12-26 13:24:25 -0300

    first commit

---
+++ src/main/java/br/com/bluesoft/desafiodevops/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package br.com.bluesoft.desafiodevops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+
+}
